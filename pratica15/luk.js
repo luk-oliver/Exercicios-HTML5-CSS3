77abce6d07b44430eb06e5f6715bd58a7446287f
Luciano de Oliveira <luciano42matos@gmail.com>
2021-12-28 07:48:31 -0300

    exercicios

---
+++ pratica15/luk.js
@@ -0,0 +1,11 @@
+// È um objeto
+let pessoa = {
+    nome: 'luciano',
+    idade: 43,
+    sobrenome: 'de Oliveira',
+    profissão: 'Advogado',
+    trabalho: 'carteiro'
+};
+console.log(pessoa)
+
+
